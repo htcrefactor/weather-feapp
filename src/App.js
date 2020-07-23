d8a58b436f985ef1314e1333d59f7ca93d49be94
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-23 11:52:21 +0900

    App.js imports BrowserRouter from react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header'
 import './App.css';
 
 function App() {
   const cityName = "Seoul";
   return (
+    <BrowserRouter>
     <div className="App">
       <Header cityName={cityName}/>
       <p>Hello World!</p>
     </div>
+    </BrowserRouter>
   );
 }
 
