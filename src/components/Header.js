2be992536997d27da2bd106d13ad08fa31271ac1
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-23 10:58:56 +0900

    App.js imports Header from './components/Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 function Header() {
     return (
@@ -18,3 +19,5 @@ function Header() {
         </header >
     );
 }
+
+export default Header;
