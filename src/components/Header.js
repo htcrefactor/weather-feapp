52aaaf50958913a5c6b8a278ab4985f0516952ed
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-23 11:30:42 +0900

    Changed Header function style from ES5 to ES6 arrow function.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,42 @@
 import logo from '../logo.svg';
 import React from 'react';
 
+// ES5 function
+/*
 function Header( props ) {
     console.log( props );
     console.log ( props.cityName );
 
     const cityName = props.cityName;
+
+    return (
+        < header className="App-header" >
+            <div>{cityName}</div>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header >
+    );
+}
+*/
+
+// ES6 arrow function
+const Header = ( props ) => {
+    console.log( props );
+    console.log ( props.cityName );
+
+    // Parameter props will be in this form
+    // props = { cityName: 'Seoul' };
+    const { cityName } = props;
 
     return (
         < header className="App-header" >
