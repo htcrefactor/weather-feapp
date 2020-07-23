116b99b3f957f5a74fbfbfce5b9c7b59803807bc
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-23 15:02:06 +0900

    과제로 이후  추가 진행될 예정

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 // Stateless component
 const Weather = (props) => {
+  
+  // 방법1.
+  // class 형태로 변경 후 fetch 선택 도시의 날씨
+
+  // 방법2. (이 방법으로 하면 가산점)
+  // react Hook << 검색
+  // state, setState
+
   const { cityName } = props.match.params;
   console.log(cityName);
   return (
