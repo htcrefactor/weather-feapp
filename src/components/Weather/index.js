1aa562673cc4ddde974983461a9542b4b34e3866
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-26 11:56:18 +0900

    Added API URI to fetch weather details.

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -4,6 +4,8 @@ const Weather = (props) => {
   
   // 방법1.
   // class 형태로 변경 후 fetch 선택 도시의 날씨
+  const API_FETCH_DETAILS = "http://localhost:8080/weather-crawler/current-weathers/by-city-name/{cityName}";
+  
 
   // 방법2. (이 방법으로 하면 가산점)
   // react Hook << 검색
@@ -20,4 +22,5 @@ const Weather = (props) => {
     </div>
   );
 };
+
 export default Weather;
