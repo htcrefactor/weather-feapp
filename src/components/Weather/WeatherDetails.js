3bf37076cc1cbf306ab220191586d02a1a925121
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-26 23:58:16 +0900

    Finished

--- src/components/Weather/WeatherDetails.js
+++ src/components/Weather/WeatherDetails.js
@@ -26,23 +26,77 @@ class WeatherDetails extends React.Component {
 
   compoenentDidMount() {
     console.log("Component Loaded");
+    const { weatherDetails } = this.state;
     const { cityName } = this.props.match.params;
-    console.log("cityName = ", cityName);
+
+    this.setState({
+      city: cityName
+    })
+
+    /*
+    const API_FETCH_URI = "http://localhost:8080/weather-crawler/current-weathers/by-city-name/";
+    console.log("Anyone there?");
+    const WeatherData = fetch(API_FETCH_URI + cityName)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          weatherMain: data.weather[0].main,
+          weatherDetails: data.weather[0].description,
+          temp: data.main.temp
+        });
+      })
+
+      .catch((err) => console.warn("Something weird just happened :: ", err));
 
     this.setState({
       cityName: cityName
     })
+    */
   }
 
   render() {
+    const { weatherDetails } = this.state;
     const { cityName } = this.props.match.params;
-    return (
-      <div>
-        This is WeatherDetails.
-        <p>City: { cityName }</p>
-      </div>
-      
-    );
+    const API_FETCH_URI = "http://localhost:8080/weather-crawler/current-weathers/by-city-name/";
+    console.log("Anyone there?");
+    const WeatherData = fetch(API_FETCH_URI + cityName)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          weatherMain: data.weather[0].main,
+          weatherDetails: data.weather[0].description,
+          temp: data.main.temp
+        });
+      })
+
+      .catch((err) => console.warn("Something weird just happened :: ", err));
+
+    
+    // const {cityName} = this.state;
+    const {weatherMain} = this.state;
+    // const {weatherDetails} = this.state;
+    const {temp} = this.state;
+    const tempConverted = (temp - 273.15).toFixed(1);
+
+    if (weatherMain == "") {
+      return (
+        <div>
+          <h2>City: {cityName}</h2>
+          <p>Something weird just happened...</p>
+        </div>
+      )
+    }
+
+    else {
+      return (
+        <div>
+          <h2>City: {cityName}</h2>
+          <p>Weather: {weatherMain}</p>
+          <p>Details: {weatherDetails}</p>
+          <p>Temperature: {tempConverted}℃</p>
+        </div>
+      )
+    }
   }
 }
 
